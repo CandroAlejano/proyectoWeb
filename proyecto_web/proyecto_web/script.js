5306abca45b2a6aa50832e975dcc9de41ce33b4a
CandroAlejano <116350014+CandroAlejano@users.noreply.github.com>
2023-05-20 17:27:58 +0200

    Add files via upload
    A partir de ahora trabajaremos con esta carpeta, ya contiene los html necesarios para empezar con los distintos perfiles

---
+++ proyecto_web/proyecto_web/script.js
@@ -0,0 +1,47 @@
+const listaDeUsuarios = [
+    { user: "profesor", password: "1234" },
+    { user: "alumno", password: "1234" },
+    { user: "admin", password: "1234" }
+];
+
+function login() {
+    var user, password;
+    var usuarioCorrecto = false;
+
+    user = document.getElementById("usuario").value;
+    password = document.getElementById("contrasenya").value;
+
+    for (let i = 0; i < listaDeUsuarios.length; i++) {
+        const usuario = listaDeUsuarios[i];
+
+        if (usuario.user === user && usuario.password === password) {
+            usuarioCorrecto = true;
+
+            if (usuario.user === "profesor") {
+
+                alert("profesor");
+                window.location = "profesor.html";
+
+            } else if (usuario.user === "alumno") {
+
+                window.location = "alumno.html";
+                alert("alumno");
+
+            } else if (usuario.user === "admin") {
+
+                window.location = "admin.html";
+                alert("admin");
+                
+            }
+        }
+    }
+
+    if (!usuarioCorrecto) {
+        alert("Usuario o contraseña incorrectos");
+    }
+}
+
+function redireccion() {
+    window.location.href = "registro.html";
+}
+
